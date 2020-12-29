953a332b7d16ea5f19dfc405468e0bf47f473054
Tchea JiaWei <tcheajiawei@Hotmail.com>
2020-12-29 16:56:56 +0800

    setup sqlalchemy

--- application.py
+++ application.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template,request
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/loreal_db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column('student_id',db.Integer,primary_key = True)
+    name = db.Column(db.String(50))
+    price = db.Column(db.Float(precision=2))
+    last_scraped = db.Column(db.DateTime)
+
+def __init__(self,name,price,last_scraped):
+    self.name = name
+    self.price = price
+    self.last_scraped = last_scraped
 
 @app.route('/')
 def index():
