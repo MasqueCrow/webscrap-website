4f04a24de237c9c63a4fbf901d3c66871aaf83cf
Tchea JiaWei <tcheajiawei@Hotmail.com>
2021-01-05 14:01:12 +0800

    Restructure code
    -move model into its own concern
    -Add user model into db

--- application.py
+++ application.py
@@ -1,22 +1,34 @@
 from flask import Flask, render_template,request
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/loreal_db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-class Product(db.Model):
-    id = db.Column('student_id',db.Integer,primary_key = True)
-    name = db.Column(db.String(50))
-    price = db.Column(db.Float(precision=2))
-    last_scraped = db.Column(db.DateTime)
-
 def __init__(self,name,price,last_scraped):
     self.name = name
     self.price = price
     self.last_scraped = last_scraped
+
+# blueprint for auth routes in our app
+from auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+# blueprint for models
+from model import model as model_blueprint
+app.register_blueprint(model_blueprint)
+
+from flask_login import LoginManager
+from model import User
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+#A user loader tells Flask-Login how to find a specific user from the ID that is stored in their session cookie
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
