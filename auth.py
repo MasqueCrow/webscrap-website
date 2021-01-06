fa6a8dd0b94d2eca942c99b14e19d3d61ba7deef
Tchea JiaWei <tcheajiawei@Hotmail.com>
2021-01-06 20:51:22 +0800

    Add authorisation feature
    -Add signup feature
    -Add login feature
    -Redesign signup and login pages
    -Include client-server validation

--- auth.py
+++ auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint,render_template,redirect, url_for, request,flash
+from flask_login import login_user,logout_user,login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from model import User
 from model import db
@@ -27,7 +28,26 @@ def signup_post():
 
     return redirect(url_for('auth.login'))
 
-@auth.route('/login')
+@auth.route('/login',methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    #check if user actually exists
+    #take the user-supplied password, harsh it, and compare it to hashed password in database
+    if not user or not check_password_hash(user.password,password):
+        flash('Please check your login details and try again.')
+        #if user doesn't exist or incorrect password,reload page
+        return redirect(url_for('auth.login'))
+
+    #if the above check passes, then we know user has right credentials
+    login_user(user,remember=remember)
+    return redirect(url_for('index'))
+
+@auth.route('/')
 def login():
     return render_template('login.html')
 
@@ -36,5 +56,7 @@ def signup():
     return render_template('signup.html')
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('auth.login'))
