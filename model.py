4b2331be11aeb18a5985a70c8ef7dd93c44702db
Tchea JiaWei <tcheajiawei@Hotmail.com>
2021-01-10 00:40:47 +0800

    Include datatable and flask-migration package
    -Datatable with multi checkbox
    -Flask-migration to handle model column changes

--- model.py
+++ model.py
@@ -1,16 +1,19 @@
-from flask_login import UserMixin
 from application import app
-from flask_sqlalchemy import SQLAlchemy
 from flask import Blueprint
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 model = Blueprint('model', __name__)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Product(db.Model):
     id = db.Column('student_id',db.Integer,primary_key = True)
     name = db.Column(db.String(50))
-    price = db.Column(db.Float(precision=2))
-    last_scraped = db.Column(db.DateTime)
+    #price = db.Column(db.Float(precision=2))
+    price = db.Column(db.Numeric(10,2))
+    last_scraped = db.Column(db.DateTime,nullable=True)
 
 # UserMixin will add Flask-Login attributes to the model so that Flask-Login will be able to work with it
 class User(UserMixin,db.Model):
